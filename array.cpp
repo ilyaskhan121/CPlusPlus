3ed9ed00876657917871053d8ea49e9371126a9c
Ilyas Khan <dp.king.pubg89@gmail.com>
2022-03-15 09:40:53 +0500

    change from hard code to runtime

--- array.cpp
+++ array.cpp
@@ -1,19 +1,16 @@
 #include<iostream>
 using namespace std;
 int main()
-{
-    int numbers[9];
-     numbers[0]=1;
-     numbers[1]=2;
-     numbers[2]=4;
-     numbers[3]=5;
-     numbers[4]=6;
-     numbers[5]=7;
-     numbers[6]=8;
-     numbers[7]=9;
-     numbers[8]=3;
-    for(int i=0;i<9;i++)
-    {
+{   
+    int size;
+    cout<<"Enter size of array : ";
+    cin>>size;
+    int numbers[size];
+    cout<<"Enter values for array : ";
+    for(int i=0;i<size;i++){
+        cin>>numbers[i];
+    }
+    for(int i=0;i<size;i++){
        numbers[i] =  numbers[i]*4;
         cout<<"value in numbers["<<i<<"]="<<numbers[i]<<endl;
     }
